f1158db8dd80f4a9c037e02891c5caf321d9d7ca
EthanFurtado <92597232+EthanFurtado@users.noreply.github.com>
2022-01-24 08:46:49 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,550 @@
+namespace SpriteKind {
+    export const door = SpriteKind.create()
+    export const foor2 = SpriteKind.create()
+    export const door2 = SpriteKind.create()
+    export const door3 = SpriteKind.create()
+    export const FightEntrance = SpriteKind.create()
+    export const MainEntrance = SpriteKind.create()
+    export const maindor = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (fight_place_door.x < 0) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 5 5 5 5 . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 1000, 0)
+    } else {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, mySprite, 99, 0)
+    }
+})
+function missed_bullet () {
+    if (projectile.x > scene.screenWidth()) {
+        projectile.destroy()
+    } else {
+    	
+    }
+}
+function list1 () {
+    text_list1 = [
+    "what is the capital of Albania",
+    "what is the capital of Japan",
+    "what is the capital of south korea",
+    "what is the capital of Germany",
+    "what is the capital of France",
+    "what is the capital of Spain",
+    "what is the capital of Portugal",
+    "what is the capital of Mexico",
+    "what is the capital of Serbia",
+    "what is the capital of Russia",
+    "what is the capital of poland",
+    "what is the capital of Peru",
+    "what is the capital of Egypt",
+    "what is the capital of Afghanistan",
+    "what is the capital of Liberia",
+    "what is the capital of Ireland",
+    "what is the capital of Netherlands",
+    "what is the capital of Denmark",
+    "what is the capital of Latvia",
+    "what is the capital of Lebanon",
+    "what is the capital of Greece",
+    "what is the capital of El Salvador",
+    "what is the capital of Guinea-Bissau",
+    "what is the capital of Costa Rica",
+    "what is the capital of Canada",
+    "what is the capital of South Sudan",
+    "what is the capital of Hungary",
+    "what is the capital of Austria",
+    "what is the capital of Cuba",
+    "what is the capital of Argentina"
+    ]
+    text_list2 = [
+    "Tirana",
+    "Tokyo",
+    "Seoul",
+    "Berlin",
+    "Paris",
+    "Madrid",
+    "Lisbon",
+    "Mexico City",
+    "Belgrade",
+    "Moscow",
+    "Warsaw",
+    "Lima",
+    "Cairo",
+    "Kabul",
+    "Monrovia",
+    "Dublin",
+    "Amsterdam",
+    "Copenhagen",
+    "Latvia",
+    "Beirut",
+    "Athens",
+    "Praia",
+    "Bissau",
+    "San Salvador",
+    "Ottawa",
+    "Juba",
+    "Budapest",
+    "Vienna",
+    "Havana",
+    "Buenos Aires"
+    ]
+}
+function snake_gone_to_far () {
+    if (bad_guy.x < 0) {
+        info.changeLifeBy(-1)
+    } else {
+        info.changeLifeBy(0)
+    }
+}
+function list3 () {
+    door3_list_1 = [
+    "what element has the atomic number of 1",
+    "what element has the atomic number of 2",
+    "what element has the atomic number of 3",
+    "what element has the atomic number of 4",
+    "what element has the atomic number of 5",
+    "what element has the atomic number of 6",
+    "what element has the atomic number of 7",
+    "what element has the atomic number of 8",
+    "what element has the atomic number of 9",
+    "what element has the atomic number of 10",
+    "what element has the atomic number of 12",
+    "what element has the atomic number of 13",
+    "what element has the atomic number of 14",
+    "what element has the atomic number of 15",
+    "what element has the atomic number of 16",
+    "what element has the atomic number of 17",
+    "what element has the atomic number of 18",
+    "what element has the atomic number of 19",
+    "what element has the atomic number of 20",
+    "what element has the atomic number of 21",
+    "what element has the atomic number of 22",
+    "what element has the atomic number of 23",
+    "what element has the atomic number of 24",
+    "what element has the atomic number of 25",
+    "what element has the atomic number of 26",
+    "what element has the atomic number of 27",
+    "what element has the atomic number of 28",
+    "what element has the atomic number of 29",
+    "what element has the atomic number of 30",
+    "what element has the atomic number of 31",
+    "what element has the atomic number of 32",
+    "what element has the atomic number of 33",
+    "what element has the atomic number of 34",
+    "what element has the atomic number of 35",
+    "what element has the atomic number of 36"
+    ]
+    door_3_list_2 = [
+    "Hydrogen",
+    "helium",
+    "Lithium",
+    "Beryllium",
+    "Boron",
+    "Carbon",
+    "Nitrogen",
+    "Oxygen",
+    "Fluorine",
+    "Neon",
+    "Sodium",
+    "Magnesium",
+    "Aluminum",
+    "Silicon",
+    "Phosphorus",
+    "Sulfur",
+    "Chlorine",
+    "Argon",
+    "Potassium",
+    "Calcium",
+    "Scandium",
+    "Titanium",
+    "Vanadium",
+    "Chromium",
+    "Manganese",
+    "Iron",
+    "Cobalt",
+    "Nickel",
+    "Copper",
+    "Zinc",
+    "Gallium",
+    "Germanium",
+    "Arsenic",
+    "Selenium",
+    "Bromine",
+    "Krypton"
+    ]
+}
+function destroybadguy () {
+    if (fight_place_door.x > 0) {
+        bad_guy.destroy()
+    }
+}
+function main_door_interaction () {
+    door_1.setFlag(SpriteFlag.Invisible, false)
+    door_1.setFlag(SpriteFlag.Ghost, false)
+    Door2.setFlag(SpriteFlag.Invisible, false)
+    Door2.setFlag(SpriteFlag.Ghost, false)
+    Door3.setFlag(SpriteFlag.Invisible, false)
+    Door3.setFlag(SpriteFlag.Ghost, false)
+    main_place_doorway.setPosition(-30, 60)
+    fight_place_door.setPosition(140, 60)
+    mySprite.setPosition(80, 60)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.door, function (sprite, otherSprite) {
+    openDoor(1)
+})
+function fightdoorhit () {
+    door_1.setFlag(SpriteFlag.Invisible, true)
+    door_1.setFlag(SpriteFlag.Ghost, true)
+    Door2.setFlag(SpriteFlag.Invisible, true)
+    Door2.setFlag(SpriteFlag.Ghost, true)
+    Door3.setFlag(SpriteFlag.Invisible, true)
+    Door3.setFlag(SpriteFlag.Ghost, true)
+    main_place_doorway.setPosition(10, 60)
+    fight_place_door.setPosition(-140, 60)
+    mySprite.setPosition(40, 46)
+}
+info.onLifeZero(function () {
+    game.over(false)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.door3, function (sprite, otherSprite) {
+    openDoor(3)
+})
+function list2 () {
+    door_2_list_1 = [
+    "what is hello in French",
+    "what is hello in German",
+    "what is hello in Italy",
+    "what is hello in Swedish",
+    "what is hello in Hawaiian",
+    "what is hello in Turkish",
+    "what is hello in polish",
+    "what is hello in Welsh",
+    "what is hello in Czech",
+    "what is hello in spanish",
+    "what is hello in Romanian",
+    "what is hello in Portuguese (no accent)",
+    "what is hello in latin",
+    "what is hello in Malagasy",
+    "what is hello in Finnish"
+    ]
+    door2_list_2 = [
+    "Bonjour",
+    "Hallo",
+    "Ciao",
+    "Hej",
+    "aloha",
+    "Merhaba",
+    "Witaj",
+    "Helo",
+    "Ahoj",
+    "Hola",
+    "Buna ziua",
+    "Ola",
+    "Salve",
+    "Salama",
+    "Hei"
+    ]
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.door2, function (sprite, otherSprite) {
+    openDoor(2)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.FightEntrance, function (sprite, otherSprite) {
+    fightdoorhit()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.maindor, function (sprite, otherSprite) {
+    main_door_interaction()
+})
+function openDoor (doorNum: number) {
+    let random_number_2 = 0
+    if (doorNum == 1) {
+        random_number = randint(1, 29)
+        list1()
+        listQuestion = text_list1[random_number]
+        listAnswer = text_list2[random_number]
+        theQuestion = listQuestion
+    } else if (doorNum == 2) {
+        random_number = randint(1, 14)
+        list2()
+        listQuestion = door_2_list_1[random_number_2]
+        listAnswer = door2_list_2[random_number_2]
+        theQuestion = listQuestion
+    } else if (doorNum == 3) {
+        random_number = randint(1, 35)
+        list3()
+        listQuestion = door3_list_1[random_number_2]
+        listAnswer = door_3_list_2[random_number_2]
+        theQuestion = listQuestion
+    } else {
+        info.changeScoreBy(0)
+    }
+    mySprite.setPosition(90, 75)
+    game.showLongText(theQuestion, DialogLayout.Bottom)
+    while (guess != listAnswer) {
+        guess = game.askForString(theQuestion)
+        if (guess != listAnswer) {
+            info.changeLifeBy(-1)
+            game.showLongText(theQuestion, DialogLayout.Bottom)
+        }
+    }
+    game.splash("Correct!")
+    info.changeScoreBy(1)
+    game.splash("you have ", "" + info.life() + " lives left")
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(1)
+    otherSprite.destroy()
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy()
+    pause(100)
+})
+let guess = ""
+let theQuestion = ""
+let listAnswer = ""
+let listQuestion = ""
+let random_number = 0
+let door2_list_2: string[] = []
+let door_2_list_1: string[] = []
+let door_3_list_2: string[] = []
+let door3_list_1: string[] = []
+let text_list2: string[] = []
+let text_list1: string[] = []
+let projectile: Sprite = null
+let bad_guy: Sprite = null
+let fight_place_door: Sprite = null
+let main_place_doorway: Sprite = null
+let Door3: Sprite = null
+let Door2: Sprite = null
+let door_1: Sprite = null
+let mySprite: Sprite = null
+// Project Requirements
+// 
+// Required: At least 1 function that "abstract" your code into components. Also that simply code that is repeated.
+// 
+// At Least 3 of the Following:
+// 
+// -Sprites and Projectiles
+// 
+// -Controller(dx) and move mySprite with yes
+// 
+// -on game update and on game update every 500 seconds
+// 
+// -while loop
+// 
+// -Functions yes
+// 
+// -tilemap
+// 
+// -ask and ask for string with text yes
+// 
+// -and and or
+// 
+// -Numeric Comparisons
+mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . a a a . . . . . . 
+    . . . . . . a f a f a . . . . . 
+    . . . . . . a a a a a . . . . . 
+    . . . . . . a f f f a . . . . . 
+    . . . . . . a a a a a . . . . . 
+    . . . . . . . a a a . . . . . . 
+    . . . . a a a a a a a a . . . . 
+    . . . a a a a a a a a a a a a . 
+    . a a a a . . a a a . . . a a a 
+    . . . . . . . a a a . . . . . . 
+    . . . . . . . a a a . . . . . . 
+    . . . . . a a a . a a . . . . . 
+    . . . . . a a . . . a a . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+door_1 = sprites.create(img`
+    . . c c c c c c c c c . . . . . 
+    . . c c c f f c c c c . . . . . 
+    . . c c f b f c c c c . . . . . 
+    . . c c b b f b c c c . . . . . 
+    . . c b b f f f b c c . . . . . 
+    . . c c b b b b c c c . . . . . 
+    . . c c c b b c c c c . . . . . 
+    . . c c c c c c c c c . . . . . 
+    . . c c c c c c c f f . . . . . 
+    . . c c c c c c c c c . . . . . 
+    . . c c c c c c c c c . . . . . 
+    . . c c b b b b c c c . . . . . 
+    . . c b b b b b b c c . . . . . 
+    . . c c b b b b b c c . . . . . 
+    . . c c c b b b c c c . . . . . 
+    . . c c c c c c c c c . . . . . 
+    `, SpriteKind.door)
+door_1.setPosition(20, 20)
+Door2 = sprites.create(img`
+    . . c c c c c c c c c . . . . . 
+    . . c c c f f c c c c . . . . . 
+    . . c c f b b f c c c . . . . . 
+    . . c c b b f b c c c . . . . . 
+    . . c c b f b b c c c . . . . . 
+    . . c c f f f f c c c . . . . . 
+    . . c c b b b b c c c . . . . . 
+    . . c c c c c c c c c . . . . . 
+    . . c c c c c c c f f . . . . . 
+    . . c c c c b c c c c . . . . . 
+    . . c c c b b b c c c . . . . . 
+    . . c c b b b b b c c . . . . . 
+    . . c c c b b b c c c . . . . . 
+    . . c c c c b c c c c . . . . . 
+    . . c c c c c c c c c . . . . . 
+    . . c c c c c c c c c . . . . . 
+    `, SpriteKind.door2)
+Door2.setPosition(80, 20)
+Door3 = sprites.create(img`
+    . . c c c f f f c c c . . . . . 
+    . . c c f b b b f c c . . . . . 
+    . . c b b b b b f c c . . . . . 
+    . . c b b f f f c c c . . . . . 
+    . . c b b b b b f c c . . . . . 
+    . . c c b b b b f c c . . . . . 
+    . . c c c f f f c c c . . . . . 
+    . . c c c c c c c c c . . . . . 
+    . . c c c c c c c f f . . . . . 
+    . . c c c c c c c c c . . . . . 
+    . . c c c b b c c c c . . . . . 
+    . . c c b b b b c c c . . . . . 
+    . . c b b b b b b c c . . . . . 
+    . . c c b b b b c c c . . . . . 
+    . . c c c b b c c c c . . . . . 
+    . . c c c c c c c c c . . . . . 
+    `, SpriteKind.door3)
+Door3.setPosition(140, 20)
+mySprite.setStayInScreen(true)
+main_place_doorway = sprites.create(img`
+    . . 2 2 2 2 2 2 2 2 2 . . . . . 
+    . . 2 2 9 9 9 9 9 2 2 . . . . . 
+    . . 2 9 9 9 9 9 9 9 2 . . . . . 
+    . . 2 9 9 9 9 9 9 9 2 . . . . . 
+    . . 2 9 9 9 9 9 9 9 2 . . . . . 
+    . . 2 2 9 9 9 9 9 2 2 . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 . . . . . 
+    . . 2 2 2 2 2 2 2 f f . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 . . . . . 
+    . . 2 2 2 9 9 2 2 2 2 . . . . . 
+    . . 2 2 9 9 9 9 2 2 2 . . . . . 
+    . . 2 9 9 9 9 9 9 2 2 . . . . . 
+    . . 2 2 9 9 9 9 2 2 2 . . . . . 
+    . . 2 2 2 9 9 2 2 2 2 . . . . . 
+    . . 2 2 2 2 2 2 2 2 2 . . . . . 
+    `, SpriteKind.maindor)
+main_place_doorway.setPosition(-30, 60)
+mySprite.setFlag(SpriteFlag.Ghost, false)
+fight_place_door = sprites.create(img`
+    . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    . . 8 8 a a a a a 8 8 . . . . . 
+    . . 8 a a a a a a a 8 . . . . . 
+    . . 8 a a a a a a a 8 . . . . . 
+    . . 8 a a a a a a a 8 . . . . . 
+    . . 8 8 a a a a a 8 8 . . . . . 
+    . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    . . 8 8 8 8 8 8 8 f f . . . . . 
+    . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    . . 8 8 8 a a 8 8 8 8 . . . . . 
+    . . 8 8 a a a a 8 8 8 . . . . . 
+    . . 8 a a a a a a 8 8 . . . . . 
+    . . 8 8 a a a a 8 8 8 . . . . . 
+    . . 8 8 8 a a 8 8 8 8 . . . . . 
+    . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    `, SpriteKind.FightEntrance)
+fight_place_door.setPosition(140, 60)
+info.setLife(3)
+bad_guy = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Enemy)
+projectile = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Projectile)
+game.onUpdate(function () {
+    snake_gone_to_far()
+    missed_bullet()
+    destroybadguy()
+})
+game.onUpdateInterval(700, function () {
+    if (fight_place_door.x < 0) {
+        bad_guy = sprites.create(img`
+            . . 7 7 7 . . 
+            . 7 f 7 f 7 . 
+            . 7 7 7 7 7 . 
+            7 f 7 7 7 f 7 
+            7 7 f f f 7 7 
+            7 7 7 7 7 7 7 
+            7 7 7 7 7 7 7 
+            `, SpriteKind.Enemy)
+        bad_guy.setPosition(140, randint(10, 100))
+        bad_guy.setVelocity(-40, 0)
+    } else {
+        bad_guy.destroy()
+    }
+})
